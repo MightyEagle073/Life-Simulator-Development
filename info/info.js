487eaa857cf3a7adf0b44c850393dd8b0809408d
TommyEareast <51252037+EareastTommy@users.noreply.github.com>
2022-09-04 18:05:56 +1000

    Format fix
    css adjusted, problem fixed

--- info/info.js
+++ info/info.js
@@ -1,50 +1,4 @@
-// this section is data section, will be link to database in future
-// these are sample guides which Oscar should put in the actual guides
-const guides = {
-	"guide 1":
-		"Guide 1 Curabitur vitae luctus neque, vitae facilisis libero. Aenean bibendum nulla sed neque blandit, quis iaculis lorem ornare. Praesent sapien urna, dignissim sit amet sagittis vel, dapibus eu nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam ut eros imperdiet sapien imperdiet porttitor id eget sem. Integer sed iaculis nisi. Pellentesque sit amet sapien diam. Donec et aliquam elit, vitae sagittis est. In hac habitasse platea dictumst. Vestibulum tincidunt eget est quis interdum. Proin commodo lectus ac posuere fringilla. Etiam tempor diam ac porta blandit. Fusce hendrerit risus eget nunc mattis, a imperdiet orci porttitor. Phasellus dictum maximus lectus nec gravida. Ut sollicitudin mollis ex, sit amet molestie magna cursus vel. Pellentesque pulvinar efficitur arcu at dignissim. Phasellus tempor, urna non porta bibendum, nibh mauris mattis velit, quis placerat nibh nunc ac massa. Sed sed malesuada arcu, sed pharetra dolor. Integer congue pharetra purus, sagittis lacinia ante pulvinar et. Nam facilisis risus eget semper finibus. Nulla condimentum venenatis augue nec pulvinar. Aenean purus diam, viverra at enim vitae, tempor scelerisque risus. Nullam erat mauris, cursus a auctor eu, finibus at ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent ac massa nulla. In ac dignissim enim, ornare ultricies mi. Duis est tellus, consequat in laoreet id, ornare quis enim. Ut sagittis, arcu sed commodo vehicula, velit magna efficitur mauris, vitae egestas arcu magna eget arcu. Nam porta in felis sed accumsan. Duis viverra ullamcorper lectus, vel dapibus libero maximus sed.",
-	"guide 2":
-		"Guide 2 Curabitur sit amet iaculis lorem. Proin leo lorem, porttitor quis libero vel, iaculis varius dolor. Phasellus ultrices sit amet augue luctus aliquam. Praesent sollicitudin eu nunc at pellentesque. Mauris mattis justo lectus, in placerat nisl venenatis tempor. Aenean ac convallis sapien. Aenean a magna nulla. Nulla mollis pulvinar dui. Etiam quis mi ipsum. Fusce vitae urna nisi. Quisque ultricies, diam in venenatis facilisis, dolor velit aliquam metus, at facilisis lacus nisi at arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in dignissim dolor. Aliquam luctus leo ac odio pretium suscipit. Vestibulum ligula urna, semper eget neque accumsan, pellentesque dictum ex. Praesent venenatis erat odio, id tristique tortor commodo nec. Etiam imperdiet, mi eu semper cursus, est magna efficitur risus, eget cursus tortor neque nec metus. In accumsan commodo tellus ut ornare. Proin sodales pellentesque nibh sit amet viverra. Morbi ultrices, tellus a auctor convallis, tortor massa tincidunt sapien, vitae aliquam nisi lectus non ex. Donec sollicitudin, orci et interdum fringilla, est risus bibendum dui, et bibendum eros tellus quis mauris. Donec urna massa, accumsan vitae vehicula eget, pretium eu tortor. Aenean mauris nunc, posuere et diam nec, porta tincidunt erat. Cras ut pretium nulla, at convallis augue.",
-	"guide 3":
-		"Guide 3 Maecenas mollis felis at risus sollicitudin luctus. Quisque lacinia rhoncus tellus. Nunc fringilla imperdiet dui, sed mattis nisl pellentesque ut. Sed volutpat efficitur ex, ac feugiat diam ultricies nec. Aliquam nec consectetur quam, quis tempor felis. In lacus ipsum, cursus eget odio ut, pharetra faucibus nibh. Praesent commodo neque lorem, vitae placerat nulla cursus in. Etiam vehicula sollicitudin suscipit. Nunc interdum volutpat viverra. Nulla pharetra nunc vitae leo volutpat, a condimentum ante rutrum. Etiam euismod elit quis quam aliquam convallis. Nam tincidunt mi diam, in placerat sem lacinia vitae. Cras maximus felis sem, eu fringilla tortor consequat sollicitudin. Vivamus euismod, urna sed sodales tempus, ipsum felis vehicula diam, non pretium lacus massa sit amet ligula. Curabitur cursus tellus condimentum nisi tincidunt pellentesque. Vivamus lorem libero, pellentesque eu molestie ac, semper vel quam. Quisque non malesuada eros. Vestibulum mattis non nisl id lacinia. Sed condimentum sapien vel sapien sollicitudin vehicula. Morbi in ornare turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed fermentum purus magna, a pharetra nibh luctus vel. Sed magna metus, blandit id nulla vel, porttitor luctus tellus. Nunc elementum, dui sed facilisis pellentesque, dui velit efficitur nunc, eu semper turpis massa sodales felis. In suscipit rhoncus justo, id convallis mauris dictum in. Pellentesque pharetra finibus luctus.",
-	"guide 4":
-		"Guide 4 Pellentesque volutpat facilisis fermentum. Praesent hendrerit sodales gravida. Mauris tincidunt iaculis ligula quis rutrum. Aliquam eleifend feugiat viverra. Duis pretium id enim imperdiet sollicitudin. Curabitur tincidunt enim quis tortor accumsan finibus. Cras nec ipsum nulla. Pellentesque nec augue vitae ligula aliquam molestie. Mauris tristique non velit at viverra. Morbi vitae vulputate nunc. Vivamus porta libero et eleifend molestie. Morbi et nunc dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam a posuere nisl. Nulla suscipit, erat a feugiat pulvinar, mauris neque scelerisque enim, non mollis mauris lacus eu lacus. Cras condimentum nec massa ac facilisis. Praesent non dui urna. Donec accumsan augue imperdiet, facilisis ipsum at, placerat quam. Pellentesque vulputate sapien ut nibh accumsan ultricies. Sed condimentum in libero at dictum. Integer congue congue nisl sed eleifend. Morbi in porttitor libero, eget blandit mi. In posuere interdum elit, sed scelerisque neque fermentum ac. Fusce a mi volutpat, tincidunt elit quis, molestie justo. Etiam ut placerat risus. Duis finibus mi eget ex feugiat, a facilisis augue pretium. Suspendisse potenti.",
-	"guide 5":
-		"Guide 5 Duis luctus mollis magna id rhoncus. Curabitur dapibus non nulla id sollicitudin. Aliquam ac tincidunt est, ut feugiat metus. Phasellus vel convallis libero, tempor ullamcorper sapien. Aenean luctus mi ut nunc aliquam porttitor. Sed at sodales lectus. Nulla maximus ipsum sit amet quam interdum, eu venenatis sem placerat. Suspendisse venenatis iaculis justo, facilisis bibendum ex efficitur vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Integer in dui varius mi commodo cursus a vel purus. In turpis eros, consequat pulvinar lectus at, pharetra dapibus elit. Aliquam sollicitudin turpis felis, at interdum risus consectetur sed. Fusce iaculis nibh eu mauris vestibulum tincidunt. Sed dictum, massa eu convallis pulvinar, ante massa suscipit orci, et pharetra dolor ipsum at urna. Vivamus at tellus orci. Sed in vulputate turpis. Pellentesque eget sapien sagittis, luctus elit nec, ullamcorper libero. Duis lobortis gravida nunc et aliquet. Donec iaculis, turpis vitae fringilla pellentesque, est quam consequat massa, et ullamcorper justo erat sit amet magna. Nam mollis, nisi ac vestibulum varius, ex nisi lacinia erat, ut tempus est nisl vitae velit.",
-	"guide 6":
-		"Guide 6 Aliquam vulputate maximus quam, sed aliquam nibh bibendum id. Morbi diam dolor, laoreet a est vel, elementum lacinia odio. Duis tempus elit sit amet nibh tempor porttitor. Nulla eget sapien a mauris fringilla condimentum dapibus eget enim. Duis non elit id leo facilisis sodales non et nisi. Vestibulum pulvinar, erat quis accumsan congue, augue erat elementum purus, ac aliquet purus velit sit amet turpis. Etiam in tincidunt massa, vitae scelerisque ipsum. Praesent semper maximus tincidunt. Aliquam accumsan sem nulla, ac porttitor elit feugiat ut. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras non purus vitae purus finibus finibus id eu enim. Curabitur eu nunc justo. Nullam rhoncus, magna nec porttitor congue, diam turpis accumsan turpis, vel scelerisque libero neque viverra dui. Maecenas id risus dictum, condimentum dolor nec, tempus neque. Quisque sapien magna, tempor sed consequat et, ornare vulputate massa. Cras commodo efficitur libero, quis sodales enim aliquet nec. Donec id massa lectus.",
-	"guide 7":
-		"Guide 7 Suspendisse urna elit, finibus ultricies felis ut, interdum bibendum lectus. Praesent porta viverra eros sit amet aliquet. Maecenas laoreet rhoncus quam, imperdiet consequat purus eleifend ut. Fusce molestie sem ac mauris ornare condimentum. In turpis sem, consequat sit amet erat ac, tristique tincidunt arcu. Maecenas tincidunt metus at mi facilisis egestas. Quisque vitae pretium nulla, non efficitur risus. Etiam mattis turpis diam, quis vehicula nisl dapibus sit amet. Donec tincidunt nibh id ex laoreet imperdiet. In id luctus erat. Fusce accumsan turpis in elit tincidunt posuere. Cras pharetra libero lorem, in varius sapien ullamcorper sit amet. Maecenas eu rhoncus tellus, sed pellentesque dui. Quisque lacinia vitae massa nec euismod. Vivamus hendrerit, tortor sed tempor tristique, felis magna malesuada libero, a tristique ipsum purus vitae mi. Aenean quis posuere ipsum. Nullam vitae convallis ante, vitae sodales ex. Donec consectetur in magna ut rutrum. Phasellus tincidunt, erat a blandit facilisis, felis velit cursus leo, et fringilla tellus orci eu nunc. Aenean sit amet arcu elementum, aliquam erat ut, pulvinar ipsum. Nam non sapien dolor. Vivamus ullamcorper, eros vel dictum lobortis, metus nulla maximus risus, at blandit mi sapien eu nisl. Phasellus vestibulum et eros quis hendrerit. In dignissim eleifend ante, in facilisis dolor tempor eget. Fusce at nisl pulvinar, iaculis urna nec, venenatis ligula. Mauris malesuada nibh neque, quis pulvinar augue mollis id. Nullam arcu justo, ullamcorper quis laoreet et, viverra eget leo. Aliquam erat volutpat.",
-	"guide 8":
-		" Guide 8: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices magna quis magna tempus, a vestibulum nibh vestibulum. Mauris nisi lectus, pharetra ut augue non, scelerisque porttitor urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi porttitor eleifend eros vitae tempor. Cras libero nunc, ultrices a faucibus at, maximus ac mi. Aenean efficitur luctus justo vel convallis. Aliquam vel venenatis lacus, eget gravida neque. Morbi lacinia volutpat ornare. Aliquam venenatis nisi id felis tristique venenatis. Fusce urna felis, dapibus at augue sit amet, scelerisque eleifend massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices magna quis magna tempus, a vestibulum nibh vestibulum. Mauris nisi lectus, pharetra ut augue non, scelerisque porttitor urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi porttitor eleifend eros vitae tempor. Cras libero nunc, ultrices a faucibus at, maximus ac mi. Aenean efficitur luctus justo vel convallis. Aliquam vel venenatis lacus, eget gravida neque. Morbi lacinia volutpat ornare. Aliquam venenatis nisi id felis tristique venenatis. Fusce urna felis, dapibus at augue sit amet, scelerisque eleifend massa.",
-	"local setting":
-		"Local Setting Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget mattis sapien, non ultrices dolor. Integer a urna pharetra, fermentum neque quis, consectetur mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc bibendum dolor eu efficitur scelerisque. Vestibulum metus nisl, varius in faucibus at, commodo sed justo. Etiam mi velit, tempus nec ex ac, tristique pellentesque neque. Curabitur eget laoreet sapien. Sed libero diam, commodo nec fringilla quis, condimentum sed arcu. Pellentesque viverra massa condimentum nisi gravida, quis lobortis nunc viverra. Sed suscipit nibh nec tortor lobortis, eu eleifend nisi pulvinar. Phasellus quis feugiat purus. Fusce ornare fringilla sapien sit amet blandit. Duis tincidunt pellentesque velit eget vehicula. Maecenas commodo arcu at tellus rutrum lobortis. Integer tempor orci vel leo ultricies, tincidunt sagittis nulla cursus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget mattis sapien, non ultrices dolor. Integer a urna pharetra, fermentum neque quis, consectetur mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc bibendum dolor eu efficitur scelerisque. Vestibulum metus nisl, varius in faucibus at, commodo sed justo. Etiam mi velit, tempus nec ex ac, tristique pellentesque neque. Curabitur eget laoreet sapien. Sed libero diam, commodo nec fringilla quis, condimentum sed arcu. Pellentesque viverra massa condimentum nisi gravida, quis lobortis nunc viverra. Sed suscipit nibh nec tortor lobortis, eu eleifend nisi pulvinar. Phasellus quis feugiat purus. Fusce ornare fringilla sapien sit amet blandit. Duis tincidunt pellentesque velit eget vehicula. Maecenas commodo arcu at tellus rutrum lobortis. Integer tempor orci vel leo ultricies, tincidunt sagittis nulla cursus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget mattis sapien, non ultrices dolor. Integer a urna pharetra, fermentum neque quis, consectetur mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc bibendum dolor eu efficitur scelerisque. Vestibulum metus nisl, varius in faucibus at, commodo sed justo. Etiam mi velit, tempus nec ex ac, tristique pellentesque neque. Curabitur eget laoreet sapien. Sed libero diam, commodo nec fringilla quis, condimentum sed arcu. Pellentesque viverra massa condimentum nisi gravida, quis lobortis nunc viverra. Sed suscipit nibh nec tortor lobortis, eu eleifend nisi pulvinar. Phasellus quis feugiat purus. Fusce ornare fringilla sapien sit amet blandit. Duis tincidunt pellentesque velit eget vehicula. Maecenas commodo arcu at tellus rutrum lobortis. Integer tempor orci vel leo ultricies, tincidunt sagittis nulla cursus.",
-};
 const guideKeys = Object.keys(guides);
-
-// credit for Developers
-const creditDevelopers = [
-	"Programming, Design and Management -- Oscar Liang",
-	"Programming and Design -- Tommy Chen",
-];
-
-// credit for Wallpapers
-const creditWallpapers = [
-	"Sunset by AxiomDesign",
-	"Forest by Mikael Gustafsson",
-	"Cityscape by Louis Coyle",
-	"Rainy by slippypenguin",
-	"Blocks by Foxerbit",
-	"Paint by Foxerbit",
-	"Moon by ProductAdda",
-	"Valley by Foxerbit",
-	"Train by nyaoayn",
-];
-// credit for Scripts
-const creditScripts = [
-	"FitText by davatron5000",
-	"Js-Cookie by Michael De Boey",
-];
 
 // select all DOM objects
 const guideTitleList = document.querySelector(".guide-title-list");
@@ -74,7 +28,7 @@ let createListItem = (parent, keys) => {
 // make guide title buttons
 window.addEventListener("DOMContentLoaded", () => {
 	// change background image
-	console.log(localStorage.getItem("settings_theme"));
+	switch_theme_tommyVersion(document.body);
 	// dynamic render list items such as guide title buttons and credits
 	createListItem(guideTitleList, guideKeys);
 	createListItem(creditListDevelopers, creditDevelopers);
@@ -104,6 +58,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	});
 });
 
+// handling guides, 1 - change the style of button, 2 - change the display guide
 let guideTitleClickHandler = (guideTitles, current, previous) => {
 	guideTitles[current].classList.add(
 		"guide-title-readed",
@@ -115,4 +70,17 @@ let guideTitleClickHandler = (guideTitles, current, previous) => {
 	// 2
 	guideParagraph.textContent = guides[guideKeys[current]];
 };
-// handling guides, 1 - change the style of button, 2 - change the display guide
+
+// !this is a function that has exact same purpose to switch_theme() in function.js, but the former is not a valid global function and should be updated
+let switch_theme_tommyVersion = (DOMobject) => {
+	DOMobject.style.backgroundImage = `url('../data/wallpapers/${
+		database["theme_names"][localStorage.getItem("settings_theme")]
+	}')`;
+};
+
+// adjust guide to fit
+let guideGlobalContainer = document.querySelector(".guide-global-container");
+let creditGlobalContainer = document.querySelector(".credit-global-container");
+let fixHeight = guideGlobalContainer.offsetHeight;
+guideGlobalContainer.style.height = fixHeight + "px";
+creditGlobalContainer.style.height = fixHeight + "px";
